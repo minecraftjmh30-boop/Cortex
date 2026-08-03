e25b075472f13c473ff6beadfa13f9131f8383a4
minecraftjmh30-boop <minecraftjmh30@gmail.com>
2026-08-03 17:01:00 -0500

    added audio test

--- command/keys/commands.py
+++ command/keys/commands.py
@@ -1,6 +1,9 @@
+from audio.play import play_audio
 from command.Command import Command
 from command.functions.lights.toggle_lights import toggle_lights
 from command.functions.record_player.toggle_record_player import toggle_record_player
 
 commands = ( Command("Toggle Lights",("lights", "toggle thy bulbs"), toggle_lights),
-             Command("Toggle Record Player",("needle drop", "drop the needle"), toggle_record_player) )
+             Command("Toggle Record Player",("needle drop", "drop the needle"), toggle_record_player),
+             Command("Audio Test", ("test audio", "audio test"), lambda: play_audio("test.mp4"))
+             )
