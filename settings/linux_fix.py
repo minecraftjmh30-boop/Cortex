4533346dd7a6302b1c0b6c81ebe5cabde6c8fca5
minecraftjmh30-boop <minecraftjmh30@gmail.com>
2026-08-06 15:31:50 -0500

    surpressed more errors while running program on linux

--- settings/linux_fix.py
+++ settings/linux_fix.py
@@ -1,4 +1,6 @@
 _c_error_handler = None
+_jack_error_handler = None
+_jack_info_handler = None
 
 def linux_fix():
     # this should suppress ALSA lib errors
@@ -18,3 +20,22 @@ def linux_fix():
         asound.snd_lib_error_set_handler(_c_error_handler)
     except OSError:
         pass
+
+    # Suppress JACK errors
+    global _jack_error_handler, _jack_info_handler
+    JACK_HANDLER_FUNC = ctypes.CFUNCTYPE(None, ctypes.c_char_p)
+
+    def py_jack_handler(msg):
+        pass
+
+    _jack_error_handler = JACK_HANDLER_FUNC(py_jack_handler)
+    _jack_info_handler = JACK_HANDLER_FUNC(py_jack_handler)
+
+    for libname in ['libjack.so.0', 'libjack.so.1']:
+        try:
+            jack = ctypes.cdll.LoadLibrary(libname)
+            jack.jack_set_error_function(_jack_error_handler)
+            jack.jack_set_info_function(_jack_info_handler)
+            break
+        except (OSError, AttributeError):
+            continue
